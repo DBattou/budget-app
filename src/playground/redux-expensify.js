0a28315726c10a321b15cb3a28838b1fc7e69649
Baptiste Doucerain <doucerain.b@gmail.com>
2018-10-25 14:28:53 +0200

    Working on expense actions and filtering using spread operator

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,17 +1,8 @@
 import { createStore, combineReducers } from "redux";
 import uuid from "uuid";
 
-// Actions
-// ADD_EXPENSE
-// REMOVE_EXPENSE
-// EDIT_EXPENSE
-// SET_TEXT_FILTER
-// SORT_BY_DATE
-// SORT_BY_AMOUNT
-// SET_START_DATE
-// SET_END_DATE
-
 // Action generators
+// ADD_EXPENSE
 const addExpense = ({
   description = "",
   note = "",
@@ -28,12 +19,61 @@ const addExpense = ({
   }
 });
 
+// REMOVE_EXPENSE
 const removeExpense = id => {
   return {
     type: "REMOVE_EXPENSE",
     id
   };
 };
+
+// EDIT_EXPENSE
+const editExpense = (id, updates) => {
+  return {
+    type: "EDIT_EXPENSE",
+    id,
+    updates
+  };
+};
+
+// SET_TEXT_FILTER
+const setTextFilter = text => {
+  return {
+    type: "SET_TEXT_FILTER",
+    text
+  };
+};
+
+// SORT_BY_AMOUNT
+const sortByAmount = () => {
+  return {
+    type: "SORT_BY_AMOUNT"
+  };
+};
+
+// SORT_BY_DATE
+const sortByDate = () => {
+  return {
+    type: "SORT_BY_DATE"
+  };
+};
+
+// SET_START_DATE
+const setStartDate = date => {
+  return {
+    type: "SET_START_DATE",
+    startDate: date
+  };
+};
+
+// SET_END_DATE
+const setEndDate = (date = undefined) => {
+  return {
+    type: "SET_END_DATE",
+    endDate: date
+  };
+};
+
 // Expenses Reducer
 const expensesReducerDefaultState = [];
 
@@ -44,6 +84,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
     case "REMOVE_EXPENSE":
       return state.filter(expense => {
         return expense.id !== action.id;
+      });
+    case "EDIT_EXPENSE":
+      return state.map(expense => {
+        if (action.id === expense.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        }
       });
     default:
       return state;
@@ -60,6 +111,16 @@ const filterReducerDefaultState = {
 
 const filterReducer = (state = filterReducerDefaultState, action) => {
   switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return { ...state, text: action.text };
+    case "SORT_BY_AMOUNT":
+      return { ...state, sortBy: "amount" };
+    case "SORT_BY_DATE":
+      return { ...state, sortBy: "date" };
+    case "SET_START_DATE":
+      return { ...state, startDate: action.startDate };
+    case "SET_END_DATE":
+      return { ...state, endDate: action.endDate };
     default:
       return state;
   }
@@ -97,9 +158,25 @@ const expenseTwo = store.dispatch(
   addExpense({ description: "condom", amount: 500 })
 );
 
-console.log(expenseOne);
-
 store.dispatch(removeExpense(expenseOne.expense.id));
+
+store.dispatch(editExpense(expenseZero.expense.id, { amount: 480 }));
+
+store.dispatch(setTextFilter("coucou"));
+
+store.dispatch(setTextFilter(""));
+
+store.dispatch(sortByAmount());
+
+store.dispatch(sortByDate());
+
+store.dispatch(setEndDate("18.05.23"));
+
+store.dispatch(setStartDate("18.05.23"));
+
+store.dispatch(setStartDate());
+
+store.dispatch(setEndDate(234));
 
 const demoState = {
   expenses: [
