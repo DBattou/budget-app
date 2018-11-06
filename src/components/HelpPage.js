54c7fe0c75866f02fcdb8c6c357ad440bfde7716
Baptiste Doucerain <baptiste.doucerain.ext@parrot.com>
2018-11-06 16:20:33 +0100

    Add documentation in help page

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const HelpPage = () => <div>This is from help page</div>;
+const HelpPage = () => (
+  <div>
+    <p>
+      <span>You can add expenses in </span>
+      <Link to={"/add"}>Add expense</Link>
+      <span> by filling the form.</span>
+    </p>
+    <p>
+      <span>You can view the expenses list in </span>
+      <Link to={"/"}>Dashboard</Link>
+    </p>
+    <p>
+      <span>You can filter the expenses by using a keyword or by choosing StartDate - EndDate</span>
+      <span>    Example: keyword = "Bill" startDate="11/01/2018" endDate="11/31/2018")</span>
+    </p>
+    <p>
+      <span>You can sort the expenses by date or by amount</span>
+    </p>
+    <p>
+      <span>Finally you can edit/remove expense by clicking an expense in  </span>
+      <Link to={"/"}>Dashboard</Link>
+    </p>
+  </div>
+)
 
 export default HelpPage;
