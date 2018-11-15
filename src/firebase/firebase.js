4b1ab15362b4b05d8578e4c35e29834711dd9ace
DBattou <doucerain.b@gmail.com>
2018-11-15 15:45:25 +0100

    Setup firebase (#1)
    * [PLAYGROUND] Database onChange subscription
    
    * [PLAYGROUND] Firebase arrays and references
    
    * [PLAYGROUND] Building reusable arrays from database
    
    * [PLAYGROUND] Building reusable arrays from database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,33 +13,79 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database
-  .ref()
-  .once("value")
-  .then(snapshot => {
-    console.log(snapshot);
-  });
-
-// database
-//   .ref()
-//   .set({
-//     name: "Battou",
-//     age: 36,
-//     isSingle: false,
-//     location: {
-//       city: "Paris",
-//       country: "Franceodf"
-//     }
-//   })
-//   .then(result => {
-//     console.log("yes its works perfectly");
-//     console.log(result);
-//   })
-//   .catch(error => {
-//     console.log("merde ça n'a pas marché");
-//     console.log(error);
-//   });
-
-// database.ref().update({
-//   "location/city": "Villennes sur seine"
+// Initial datas
+//var expenseListRef = firebase.database().ref('expense_list');
+//var newMessageRef = expenseListRef.push({
+// expenseListRef.push({
+//   description: "Sandwich",
+//   note: "",
+//   amount: 1589,
+//   createdAt: 1541026800000
 // });
+
+// expenseListRef.push({
+//   description: "Condom",
+//   note: "",
+//   amount: 476,
+//   createdAt: 1541026800000
+// });
+
+// expenseListRef.push({
+//   description: "RDR2",
+//   note: "",
+//   amount: 234,
+//   createdAt: 1541026800000
+// });
+
+/*
+// Creating an array using once()
+database.ref("expense_list").once("value").then((snapshot) => {
+  // console.log(snapshot.val())
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot) => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    })
+  })
+})
+*/
+
+/*
+// Creating an array on data changes. 
+// ==> Immutability: should return a brand new array each time data has changed
+database.ref("expense_list").on("value", (snapshot) => {
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot) => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    })
+  })
+
+  console.log(expenses)
+  return expenses;
+})
+*/
+
+// Useful methods : on child removed
+database.ref("expense_list").on("child_removed", (snapshot) => {
+  console.log("child removed", snapshot.key, snapshot.val())
+})
+
+// Useful methods : on child changed
+database.ref("expense_list").on("child_changed", (snapshot) => {
+  console.log("child changed", snapshot.key, snapshot.val())
+})
+
+// Useful methods : Child added
+// Useful methods : Child moved
+
+// We've appended a new message to the message_list location.
+// var path = newMessageRef.toString();
+// path will be something like
+// 'https://sample-app.firebaseio.com/message_list/-IKo28nwJLH0Nc5XeFmj'
+
+// More infos : https://firebase.google.com/docs/reference/js/firebase.database.Reference#on
