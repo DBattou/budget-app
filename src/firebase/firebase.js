feb4f1abf1eaf5f69595d281f7b61d0cc7ae49b1
Baptiste Doucerain <baptiste.doucerain.ext@parrot.com>
2018-12-04 17:42:51 +0100

    [TESTS] Creating separate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDPLbM-anDXSRDANfki0QVzZmm1LRHzu3Y",
-  authDomain: "budgetapp-7f3ff.firebaseapp.com",
-  databaseURL: "https://budgetapp-7f3ff.firebaseio.com",
-  projectId: "budgetapp-7f3ff",
-  storageBucket: "budgetapp-7f3ff.appspot.com",
-  messagingSenderId: "254617785073"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL:  process.env.FIREBASE_DATABASE_URL,
+  projectId:  process.env.FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
